d9c48d1012217ebd3d74f695021bd43aed6beab6
MarcoGlauser <marco.glauser@gmail.com>
2020-02-17 18:34:47 +0100

    add docker build as test

---
+++ build/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+if ! docker pull $GCR_IMAGE:$GITHUB_SHA;
+then
+  echo $GCR_IMAGE:$GITHUB_REF_SLUG $GCR_IMAGE:master $GCR_IMAGE:stage | xargs -P10 -n1 docker pull || true && \
+  docker build \
+      --cache-from=$GCR_IMAGE:master \
+      --cache-from=$GCR_IMAGE:stage \
+      --cache-from=$GCR_IMAGE:$GITHUB_REF_SLUG \
+      --build-arg PROJECT_NAME=$PROJECT_NAME \
+      -t $GCR_IMAGE:$GITHUB_SHA .
+fi
