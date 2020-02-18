ed54ffce2e922ee898147a21ef1be78a7772e96b
MarcoGlauser <marco.glauser@gmail.com>
2020-02-18 11:34:48 +0100

    use gcloud image to configure docker

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-gcloud auth configure-docker
+if [ -n "${GCP_SERVICEACCOUNT_KEY}" ]; then
+  echo "Logging into gcr.io with GCLOUD_SERVICE_ACCOUNT_KEY..."
+  echo ${GCP_SERVICEACCOUNT_KEY} | base64 --decode --ignore-garbage > /tmp/key.json
+  gcloud auth activate-service-account --quiet --key-file /tmp/key.json
+  gcloud auth configure-docker --quiet
+else
+  echo "GCLOUD_SERVICE_ACCOUNT_KEY was empty, not performing auth" 1>&2
+fi
 
 if ! docker pull $GCR_IMAGE:$GITHUB_SHA;
 then
