05f942ab3978de8a2f61fd2acb757d0dc6b1a44b
MarcoGlauser <marco.glauser@gmail.com>
2020-02-27 13:31:39 +0100

    add envsubst to fix env variables

--- slack/entrypoint.sh
+++ slack/entrypoint.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 DEFAULT_TEXT="Build for ${GITHUB_REPOSITORY} ${ENVIRONMENT} started. <https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}| Check the progress on github>"
-TEXT=${TEXT:=$DEFAULT_TEXT}
 
-curl -X POST --data-urlencode "payload={ \"text\": \"${TEXT}\", }" ${SLACK_WEBHOOK}
+TEXT=${TEXT:=$DEFAULT_TEXT}
+TEXT=$(echo $TEXT | envsubst)
+
+echo $TEXT
