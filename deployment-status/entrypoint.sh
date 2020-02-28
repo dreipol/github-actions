bc6a4b861aa4207e360e8bbad8b905ee03e43578
MarcoGlauser <marco.glauser@gmail.com>
2020-02-28 10:41:21 +0100

    add sleep of 5 sec to make sure the deployment started

--- deployment-status/entrypoint.sh
+++ deployment-status/entrypoint.sh
@@ -3,4 +3,6 @@
 set -e
 
 oc login ${OPENSHIFT_HOST} --token=${OPENSHIFT_TOKEN}
+echo "Wait 5 Seconds to check rollout progress"
+sleep 5
 oc -n ${OPENSHIFT_PROJECT} rollout status dc/${DEPLOYMENT_CONFIG}
