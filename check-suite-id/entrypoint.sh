67d0609557363610fa67771658f2f82fe0fd53c1
MarcoGlauser <marco.glauser@gmail.com>
2020-03-12 17:38:04 +0100

    rename check-suite-id

--- check-suite-id/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-GITHUB_CHECK_SUITE_ID=$(curl -s https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID} | jq -r '.check_suite_id')
-echo "::set-env name=GITHUB_CHECK_SUITE_ID::${GITHUB_CHECK_SUITE_ID}"
