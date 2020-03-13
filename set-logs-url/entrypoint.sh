df7e97dcccba53e5f8ec292ca4600a5f8beeca2b
MarcoGlauser <marco.glauser@gmail.com>
2020-03-13 10:59:16 +0100

    set curl silent

--- set-logs-url/entrypoint.sh
+++ set-logs-url/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 
-JOB_ID=$(curl  -L -H "Authorization: Bearer ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs | jq ".jobs[] | select(.name == \"${JOB_NAME}\") | .id")
+JOB_ID=$(curl -s -L -H "Authorization: Bearer ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs | jq ".jobs[] | select(.name == \"${JOB_NAME}\") | .id")
 LOGS_URL="https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks/${JOB_ID}/logs"
 echo "::set-env name=DREIPOL_JOB_ID::${JOB_ID}"
 echo "::set-env name=DREIPOL_LOGS_URL::${LOGS_URL}"
