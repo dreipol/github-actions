2dc5db26a7198ace4bbd540a14df9b8a4c87050c
MarcoGlauser <github@mglauser.ch>
2022-08-12 18:42:58 +0200

    Log infos when kustomize config can't be found.

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -20,8 +20,11 @@ fi
 
 CONFIG_DIRECTORY="deployment-config/${DEPLOY_ENVIRONMENT}"
 if test -d "$CONFIG_DIRECTORY"; then
-    oc login --server=${OPENSHIFT_HOST} --token=${OPENSHIFT_TOKEN}
-    oc apply -k $CONFIG_DIRECTORY
+  echo "Kustomize config found. Applying it to openshift..."
+  oc login --server=${OPENSHIFT_HOST} --token=${OPENSHIFT_TOKEN}
+  oc apply -k $CONFIG_DIRECTORY
+else
+  echo "Could not find kustomize configuration in ${CONFIG_DIRECTORY}"
 fi
 
 docker pull $GCR_IMAGE:$GITHUB_SHA
