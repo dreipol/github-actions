702b5cc991684c6f102452ae87b8c08b73ddc5f2
MarcoGlauser <github@mglauser.ch>
2022-08-19 16:02:30 +0200

    configure docker auth also for eu-west6 because it's not added by default

--- cleanup/entrypoint.sh
+++ cleanup/entrypoint.sh
@@ -7,6 +7,7 @@ if [ -n "${GCP_SERVICEACCOUNT_KEY}" ]; then
   echo ${GCP_SERVICEACCOUNT_KEY} | base64 -d > /tmp/key.json
   gcloud auth activate-service-account --quiet --key-file /tmp/key.json
   gcloud auth configure-docker --quiet
+  gcloud auth configure-docker eu-west6-docker.pkg.dev --quiet
 else
   echo "GCLOUD_SERVICE_ACCOUNT_KEY was empty, not performing auth" 1>&2
 fi
