83d5cef28070c8d457c3d99c5bc78fc95e8ccddd
Philipp Läubli <philipp.laeubli@gmail.com>
2024-11-20 20:29:38 +0100

    Add working directory env variable

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [ -n "${WORKING_DIRECTORY}" ]; then
+  cd $WORKING_DIRECTORY
+fi
+
 CONFIG_DIRECTORY="deployment-config/${DEPLOY_ENVIRONMENT}"
 if test -d "$CONFIG_DIRECTORY"; then
   echo "Kustomize config found. Applying it to openshift..."
